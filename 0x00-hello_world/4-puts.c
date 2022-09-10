a09fa1ae30d111e0853ff34a38a4de46b625eb94
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-10 12:44:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return:Always o (Success)
  */
 int main(void)
-{	puts("\Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
