05d6cf6d3604887d997087e951f5835deb635065
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-11 13:59:02 -0700

    -alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point 
+ * main-Entry point
  * Description: prints the alphabets in lowercase/n
- * Return: Always 0 (success) 
+ * Return: Always 0 (success)
  */
  int main(void)
  {
