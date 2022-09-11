a31953a3864b8ca4456c98c302ad67fa74e5d958
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-11 12:34:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include<unistd.h>
 /**
  *
- * *main - Entry point
+ *main-Entry point
  *
- * *Return: returns 1
+ *Return: returns 1
  *
- * */
+ **/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-}
