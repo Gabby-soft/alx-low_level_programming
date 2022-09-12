5a1d7132fa5b920fae375480ffa41de60b2f83c7
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-11 20:21:12 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point 
- * Description: prints lowercase alphabets 
+ * main - Entry point
+ * Description: prints lowercase alphabets
  * Return: Always 0 (success)
  */
 int main(void)
