35d9fc60b9e806285654d8d91b431e29c31c9ff1
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-13 19:34:16 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,8 @@
 /**
  * print_alphabet - Entry point
  * Description: a function that prints the alphabet,
- * in lowercase followed by a new line 
- * Return: void 
+ * in lowercase followed by a new line
+ * Return: void
  */
 void print_alphabet(void)
 {
