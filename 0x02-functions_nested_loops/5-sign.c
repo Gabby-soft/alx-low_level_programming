f4b347574cd7c2c82d9ae5abeb6d35fc202d3219
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-13 20:11:25 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - entry point
  * @n: carrier variable
  * Description: prints the sign of a number
- * return: 1 if n>0, 0 if n ==0, -1 if n<0
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
  */
 int print_sign(int n)
 {
