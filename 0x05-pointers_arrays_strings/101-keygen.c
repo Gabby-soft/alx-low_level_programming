07be223b24edc890da2f0e7fc19320bd2dbe83c5
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-20 21:12:31 -0700

    valid password for keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define G_MIN 33
+#define G_MAX 126
+/**
+ * main - generate passwords for 101-crackme that are valid
+ * Return: nothing with run success
+ */
+
+int main(void)
+{
+	int sum = 5789;
+	char c;
+
+	srand(time(NULL));
+	while (sum > G_MAX)
+	{
+		c = rand() % (G_MAX - G_MIN) + G_MIN;
+		sum -= c;
+		if (sum < G_MIN)
+		{
+			c -= (G_MIN - sum);
+			sum = G_MIN;
+		}
+		putchar(c);
+	}
+	putchar(sum);
+	return (0);
+}
