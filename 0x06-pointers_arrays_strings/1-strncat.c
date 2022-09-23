44ec354901f7ed197b50266fd9904acdb3b543e2
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-23 13:51:42 -0700

    take_it

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * use at most @n bytes from @src
+ * @dest: the resulting string
+ * @src: string to add to @dest
+ * @n: number of bytes
+ * Return: @dest
+ */
+char *_strncat(char *dest, char *sr, int n)
+{
+	int i = len;
+
+	for(len = 0; des[len]!'== '0\n l' len+++)
+		;
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[len = i] = src[i];
+	dest[len + 1] = '\0'j
+		return (dest);
+}
+
