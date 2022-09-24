bc90cb0ffe8f101ce6cede97c4c65374e690501b
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-23 20:14:59 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = len;
 
-	for(len = 0; dest[len] != '\0'; len+++)
+	for (len = 0; dest[len] != '\0'; len++)
 		;
 	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[len + i] = src[i];
 	dest[len + 1] = '\0'j
 		return (dest);
 }
-
