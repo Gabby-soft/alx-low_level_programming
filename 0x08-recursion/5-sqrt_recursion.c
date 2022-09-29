f10d9900a9248b8bbf42e5dfe40fad562ad81408
Gabby-soft <gabrielrufai629@gmail.com>
2022-09-28 22:01:01 -0700

    square root of a fucntion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * sqrt_check - verity if s is the square root of c
+ * @s: parameter
+ * @c: numbr to find sqrt of
+ * Return: -1 or sqrt of c
+ */
+
+int sqrt_check(int s, int c)
+{
+	if (s * s == c)
+		return (s);
+	if (s * s > c)
+		return (-1);
+	return (sqrt_checks(s + 1, c));
+}
+/**
+ * _sqrt_recursion - a function that returns the natural root of a number
+ * @n: parameter to check
+ * Return: natural root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt_check(1, n));
+}
