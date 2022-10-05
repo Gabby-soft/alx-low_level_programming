16780392c95c0e51df5d1366ce8c5b40088170e9
Gabby-soft <gabrielrufai629@gmail.com>
2022-10-05 10:03:05 -0700

    argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * main - a programme name
+ * @argc: argument count
+ * @argv: argumen vector
+ *
+ * Return: 0 success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", arvc[]);
+	return (0);
+}
