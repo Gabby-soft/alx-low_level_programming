684de91024f0b1a4506cce7bab5e1d24cbc07b05
Gabby-soft <gabrielrufai629@gmail.com>
2022-10-05 10:44:58 -0700

    number of augument pass

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,8 @@
  * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  *
- * return: 0 success;
+ * return: 0 success
  */
-
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
